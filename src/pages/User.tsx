49367aab77473805ebc04aff15811bae5cc95cfa
Aiman <aiman.ahmedpro@gmail.com>
2023-03-24 17:08:37 +0100

    set redux / edit name

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,9 +1,17 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Account from "../components/Account";
-
+import { selectFirstName, selectLastName, updateFirstName, updateLastName } from "../features/user/userSlice";
+import { useAppSelector, useAppDispatch } from "../app/hooks";
 function User() {
 
-  const accounts = useMemo( () => [
+  const firstName = useAppSelector(selectFirstName);
+  const lastName = useAppSelector(selectLastName);
+  const dispatch = useAppDispatch();
+
+  const [editFirstName, setEditFirstName] = useState<string>('');
+  const [editLastName, setEditLastName] = useState<string>('');
+  const [showEdit, setShowEdit] = useState<boolean>(false)
+  const accounts = useMemo(() => [
     {
       title: "Argent Bank Checking (x8349)",
       amount: "2,082.79",
@@ -29,18 +37,45 @@ function User() {
     };
   });
 
+  function handleChange() {
+    if (editFirstName.length) {
+      dispatch(updateFirstName(editFirstName))
+    }
+    if (editLastName.length) {
+      dispatch(updateLastName(editLastName))
+    }
+    setShowEdit(false)
+  }
+
   return (
     <>
       <div className="header">
         <h1>
           Welcome back
           <br />
-          Tony Jarvis!
+          {firstName} {lastName}!
         </h1>
-        <button className="edit-button">Edit Name</button>
+        <button onClick={() => setShowEdit(!showEdit)} className="edit-button">Edit Name</button>
+        {
+          showEdit ?
+            <>
+              <div className="edit-inputs">
+                <input onChange={(e) => setEditFirstName(e.target.value)} type='text' placeholder={firstName} />
+                <input onChange={(e) => setEditLastName(e.target.value)} type='text' placeholder={lastName} />
+              </div>
+              <div className="edit-actions">
+                <button onClick={() => handleChange()}>Save</button>
+                <button onClick={() => setShowEdit(false)}>Cancel</button>
+              </div>
+            </>
+            :
+            null
+        }
+
+
       </div>
       <h2 className="sr-only">Accounts</h2>
-      {accounts.map((account, index) => ( 
+      {accounts.map((account, index) => (
         <Account {...account} key={index} />))}
     </>
   );
